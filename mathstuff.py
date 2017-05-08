fccfad6910b9b755171e0125025506bf79ad6d82
robfacella <rob.facella@gmail.com>
2017-05-07 23:46:39 -0400

    Some math

---
+++ mathstuff.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+add = 1 + 1
+
+sub = add - 1
+
+mult = add * 2
+
+div = times / 4
+
+exponent = 2 ** 6
+#2 to the 6th
+modulo = 3%2
+#get remainder
+
---
+++ tip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+meal = 44.50
+tax = 0.07
+tip = 0.18
+#
+bill = meal + meal * tax
+total = bill + meal * tip
+#Print to 2 decimal places.
+print("%.2f" % total)
