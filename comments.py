dbdb66d4d9f8a28c1f781201c2ad802d1e57e8a7
robfacella <rob.facella@gmail.com>
2017-05-07 23:27:06 -0400

    Comments

---
+++ comments.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def funct():
+  print ("Use # for single line comments or \"\"\"  \"\"\"  for multiple lines of comments.")
+  return
+"""
+You
+Cannot
+See
+These
+"""
+funct()
+print ("I would use more comments but it ruins my golf game.")
+#or this. :^)
+print ("PersonalNote: Program that plays code golf for you?")
